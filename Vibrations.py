c332f9f8d88f244c64c07195e22d4f59e6d761a0
sonpipe0 <ffernandezcronenbol@mail.austral.edu.ar>
2023-11-03 15:46:56 -0300

    crea grafico de velocidad y timepo. Problema en la definicion de velocidad y tiempo todavia muestra valores en un rango

--- Vibrations.py
+++ Vibrations.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+from typing import List
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,7 +12,7 @@ P = Decimal(0.7890929760279801)
 c = Decimal(1)
 
 
-def force_periodic(x, P, F): #gives the value of F in x time
+def force_periodic(x, P, F):  # gives the value of F in x time
     normalized_x = Decimal(x) % P  # Normalize x to the range [0, P)
     if x < 0: return Decimal(0)
     if normalized_x < P / 10:
@@ -29,27 +31,48 @@ class Vibrations:
 
     t_points = np.linspace(0, float(2 * P), 1000)
     F_points = [force_periodic(xi, P, F) for xi in t_points]
-    x_points = [0]
-    v_points = [0]
+    x_points: List[Decimal] = [Decimal(0)]
+    v_points: List[Decimal] = [Decimal(0)]
     a_points = [0]
+
+    def f(self, i):
+        if i < 0: return 0
+        if i == 0: return F / m
+        return self.F_points[i] / m - K * self.x_points[i] - c * self.v_points[i]
+
+    def Adams_BashfordSpeed(self, i):
+        if i <= 0:
+            self.v_points.append((P / 1000) * (3 * self.f(i - 1) - self.f(i - 2)))
+        else:
+            self.v_points.append(self.v_points[i - 1] + (P / 1000) * (3 * self.f(i - 1) - self.f(i - 2)))
+
+    def Adam_BashfordXpos(self, i):
+        if i <= 0:
+            self.x_points.append((P / 1000) * (3 * self.v_points[i - 1] - self.v_points[i - 2]))
+        else:
+            self.x_points.append(self.x_points[i - 1] + (P / 1000) * (3 * self.v_points[i - 1] - self.v_points[i - 2]))
+
+    def calculate(self):
+        for x in range(len(self.t_points) - 1):
+            self.Adams_BashfordSpeed(x)
+            self.Adam_BashfordXpos(x)
+
     def graph(self):
         plt.plot(self.t_points, self.F_points, label='Force')
         plt.xlabel('Time')
         plt.ylabel('Force')
         plt.legend()
         plt.show()
-
-    def f(self,i):
-        if i<0: return 0
-        return self.F_points[i]/m-K*self.x_points[i]-c*self.v_points[i]
-
-    def Adams_BashfordSpeed(self,i):
-        self.v_points.append(self.v_points[i-1]+(P/1000)*(3*self.f(i-1)-self.f(i-2)))
-
-
+        plt.plot(self.t_points, self.v_points, color='blue', label='V(t)')
+        plt.plot(self.t_points, self.x_points, color='Red', label='X(t)')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':
     vibrations = Vibrations(m, K, x_adm, P, F)
+    vibrations.calculate()
+    print(len(vibrations.t_points))
+    print(len(vibrations.x_points))
+    print(len(vibrations.v_points))
     vibrations.graph()
-
