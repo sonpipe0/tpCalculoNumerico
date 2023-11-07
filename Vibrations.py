bc30a9cd21c9a045d7702b4d4a8713b8e317c601
sonpipe0 <ffernandezcronenbol@mail.austral.edu.ar>
2023-11-07 19:54:50 -0300

    finalized code

--- Vibrations.py
+++ Vibrations.py
@@ -1,3 +1,5 @@
+import decimal
+import time
 from decimal import Decimal
 import decimal as d
 from typing import List
@@ -6,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 m = Decimal(9700)
-K = Decimal(615)
+K = Decimal(615000)
 F = Decimal(850)
 x_adm = Decimal(0.00125)
 P = Decimal(0.7890929760279801)
@@ -24,25 +26,25 @@ def force_periodic(x, P, F):  # gives the value of F in x time
 
 
 class Vibrations:
-    def __init__(self, m, K, x_adm, P, F, c):
+    def __init__(self, m, K, P, F, c):
         self.m = m
         self.K = K
         self.x_adm = x_adm
         self.P = P
         self.F = F
         self.c = c
-        self.t_points = np.linspace(0, float(2 * P), 1000)
+        self.t_points = np.linspace(0, float(10 * P), 5000)
         self.F_points = [force_periodic(xi, P, F) for xi in self.t_points]
         self.x_points: List[Decimal] = [Decimal(0)]
         self.v_points: List[Decimal] = [Decimal(0)]
-        self.a_points = [0]
+        self.a_points = []
 
     def f(self, i):
         if i < 0:
             return 0
         if i == 0:
             return self.F / self.m
-        return self.F_points[i] / self.m - self.K * self.x_points[i] - self.c * self.v_points[i]
+        return (self.F_points[i] - self.K * self.x_points[i] - self.c * self.v_points[i]) / self.m
 
     def Adams_BashfordSpeed(self, i):
         if i <= 0:
@@ -60,6 +62,7 @@ class Vibrations:
         for x in range(1, len(self.t_points)):
             self.Adams_BashfordSpeed(x)
             self.Adam_BashfordXpos(x)
+        self.a_points = [self.f(i) for i in range(len(self.t_points))]
 
     def graph(self):
         plt.plot(self.t_points, self.F_points, label='Force')
@@ -67,8 +70,11 @@ class Vibrations:
         plt.ylabel('Force')
         plt.legend()
         plt.show()
+        plt.axhline(y=x_adm, color='red', linestyle='dashed')
+        plt.axhline(y=-x_adm, color='red', linestyle='dashed')
         plt.plot(self.t_points, self.v_points, color='blue', label='V(t)', drawstyle='steps')
         plt.plot(self.t_points, self.x_points, color='Red', label='X(t)', drawstyle='steps')
+
         plt.legend()
         plt.show()
 
@@ -80,11 +86,7 @@ class Vibrations:
         return x_max
 
     def g(self):
-        x_max: Decimal = d.Decimal(0)
-        for x in self.x_points:
-            if x.copy_abs() > x_max:
-                x_max = x.copy_abs()
-        return x_max - x_adm
+        return self.x_max() - x_adm
 
     def secantMethod(self, cAnt, gAnt):
         nextC = self.c - ((self.g()) * (self.c - cAnt) / (self.g() - gAnt))
@@ -92,21 +94,21 @@ class Vibrations:
 
 
 if __name__ == '__main__':
-    cList: List[Decimal] = [Decimal(0.001), Decimal(0.5)]
+    cList: List[Decimal] = [Decimal(0.001), Decimal(19000)]
     i: int = 1
-    while (cList[i] - cList[i - 1]).copy_abs() > 0.001:
-        vibrations = Vibrations(m, K, x_adm, P, F, cList[i])
+    while (cList[i] - cList[i - 1]).copy_abs() > 0.001 or newvibrations.x_max() > x_adm:
+        vibrations = Vibrations(m, K, P, F, cList[i])
         vibrations.calculate()
         vibrations.graph()
-        VibrationsAnterior = Vibrations(m, K, x_adm, P, F, cList[i - 1])
+        VibrationsAnterior = Vibrations(m, K, P, F, cList[i - 1])
         VibrationsAnterior.calculate()
         gAnterior = VibrationsAnterior.g()
         print(vibrations.x_max())
         cList.append(vibrations.secantMethod(cList[i - 1], gAnterior))
         i += 1
-        newvibrations = Vibrations(m, K, x_adm, P, F, cList[i])
+        newvibrations = Vibrations(m, K, P, F, cList[i])
         newvibrations.calculate()
-    finalVibrations = Vibrations(m, K, x_adm, P, F, cList[len(cList) - 1])
+    finalVibrations = Vibrations(m, K, P, F, cList[len(cList) - 1])
     print(cList[len(cList) - 1])
     print(cList)
     finalVibrations.calculate()
